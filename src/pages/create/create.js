cf9272cd955af311e7dcc2cc8d39a6cd44dd1c25
EliRoCas <elirocas946@gmail.com>
2024-09-01 18:33:31 -0500

    add: base function save js

--- src/pages/create/create.js
+++ src/pages/create/create.js
@@ -1,4 +1,32 @@
-let sum = 1 + 1;
-console.log(sum); 
+const noteForm = document.getElementById("noteForm");
 
-alert('Prueba JS SPA')
+function save(event) {
+  event.preventDefault();
+
+  const formData = new FormData(noteForm);
+  const formValues = Object.fromEntries(formData.entries());
+
+  noteRepository.save(formValues);
+}
+
+
+// This is a first aproach, with the event listener direct on js  
+// const noteForm = document.getElementById("noteForm");
+
+// noteForm.addEventListener("submit", function (event) {
+//   event.preventDefault();
+
+//   const formData = new FormData(noteForm);
+//   const formValues = Object.fromEntries(formData.entries());
+
+//   alert(JSON.stringify(formValues));
+//   //   localStorage.setItem("note", JSON.stringify(note));
+//   //   debugger;
+// });
+
+//   alert(JSON.stringify(formValues));
+//   localStorage.setItem("note", JSON.stringify(formValues));
+//   debugger;
+
+//   noteRepository.save(formValues);
+// }
